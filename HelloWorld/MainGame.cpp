fa5777522e275095023df9f9c59e28aa93b004e0
z0diack <jakevardy@gmail.com>
2023-10-23 21:42:35 +0100

    Intro to C++
    Changes the display resolution, Creates function to iterate through all the different available sprites, sets up GameState object.

--- HelloWorld/MainGame.cpp
+++ HelloWorld/MainGame.cpp
@@ -2,26 +2,53 @@
 #define PLAY_USING_GAMEOBJECT_MANAGER
 #include "Play.h"
 
-int DISPLAY_WIDTH = 640;
-int DISPLAY_HEIGHT = 360;
-int DISPLAY_SCALE = 2;
+int DISPLAY_WIDTH = 1280;
+int DISPLAY_HEIGHT = 720;
+int DISPLAY_SCALE = 1;
 
-// The entry point for a PlayBuffer program
+struct GameState 
+{
+	int score = 0;
+};
+
+GameState gameState;
+
+enum GameObjectTupe {
+
+};
+
+
+// The entry point for a PlayBuffer program, Start/Awake
 void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
 {
 	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
 }
 
-// Called by PlayBuffer every frame (60 times a second!)
+// Called by PlayBuffer every frame (60 times a second!), same as update in Unity
 bool MainGameUpdate( float elapsedTime )
 {
+	// timer += Time.deltaTime in unity
+	gameState.timer += elapsedTime;
 	Play::ClearDrawingBuffer( Play::cOrange );
+
+	// Changed where the sprites are being loaded for now.
+	Play::DrawDebugText(
+		{ DISPLAY_WIDTH / 3, DISPLAY_HEIGHT / 3 },
+		Play::GetSpriteName(gameState.spriteId),
+		Play::cWhite);
+
+	Play::DrawSprite(gameState.spriteId, Play::GetMousePos(), gameState.timer);
+
+	if (Play::KeyPressed(VK_SPACE)) {
+		gameState.spriteId++;
+	}
+
 	Play::DrawDebugText( { DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, "Hello World!" );
 	Play::PresentDrawingBuffer();
 	return Play::KeyDown( VK_ESCAPE );
 }
 
-// Gets called once when the player quits the game 
+// Gets called once when the player quits the game, Destroy
 int MainGameExit( void )
 {
 	Play::DestroyManager();
