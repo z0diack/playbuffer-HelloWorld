2d0fbc5e5d8dc524e4968bbddbeed631cf9ff3a0
z0diack <jakevardy@gmail.com>
2023-10-24 20:23:06 +0100

    Stage 4

--- HelloWorld/MainGame.cpp
+++ HelloWorld/MainGame.cpp
@@ -13,12 +13,27 @@ struct GameState
 
 GameState gameState;
 
-enum GameObjectTupe {
+enum GameObjectTupe 
+{
 	TYPE_NULL = -1,
 	TYPE_AGENT8,
+	TYPE_FAN,
+	TYPE_TOOL,
+	TYPE_COIN,
+	TYPE_STAR,
+	TYPE_LASER,
+	TYPE_DESTROYED,
 };
 
+// Function to handle player movement
 void HandlePlayerControls();
+
+// Function to update the fan in-game
+void UpdateFan();
+
+// Function to update the tools in-game
+void UpdateTools();
+
 
 // The entry point for a PlayBuffer program, Start/Awake
 void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
@@ -28,6 +43,12 @@ void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
 	Play::LoadBackground("Data\\Backgrounds\\background.png");
 	Play::StartAudioLoop("music");
 	Play::CreateGameObject(TYPE_AGENT8, { 115,0 }, 50, "agent8");
+	
+	// Instantiate in unity
+	int id_fan = Play::CreateGameObject(TYPE_FAN, { 1140,217 }, 0, "fan");
+	Play::GetGameObject(id_fan).velocity = { 0,3 };
+	Play::GetGameObject(id_fan).animSpeed = 1.0f;
+
 }
 
 // Called by PlayBuffer every frame (60 times a second!), same as update in Unity
@@ -35,6 +56,8 @@ bool MainGameUpdate( float elapsedTime )
 {
 	Play::DrawBackground();
 	HandlePlayerControls();
+	UpdateFan();
+	UpdateTools();
 	Play::PresentDrawingBuffer();
 	return Play::KeyDown( VK_ESCAPE );
 }
@@ -51,6 +74,9 @@ void HandlePlayerControls()
 	// Loads in the Agent GameObject
 	GameObject& obj_agent8 = Play::GetGameObjectByType(TYPE_AGENT8);
 
+
+	// Spider like movement, down is much faster than going up.
+	
 	// If up arrow is pressed, the agent will change sprite to the climbing animation, as well as change velocity to go up.
 	if (Play::KeyDown(VK_UP)) 
 	{
@@ -86,3 +112,79 @@ void HandlePlayerControls()
 	// Slower than DrawObject but is used if the rotation/scale is required.
 	Play::DrawObjectRotated(obj_agent8);
 }
+
+void UpdateFan() 
+{
+	// Gets the fan
+	GameObject& obj_fan = Play::GetGameObjectByType(TYPE_FAN);
+	// Draws it into the scene
+	Play::DrawObject(obj_fan);
+
+	// Generates random number between 1-50 each frame, if its 50 it spawns driver and pushes towards the player.
+	if (Play::RandomRoll(50) == 50)
+	{
+		int id = Play::CreateGameObject(TYPE_TOOL, obj_fan.pos, 50, "driver");
+		GameObject& obj_tool = Play::GetGameObject(id);
+		obj_tool.velocity = Point2f(-8, Play::RandomRollRange(-1, 1) * 6);
+
+		// Then rolls a 50/50 to decide if its going to be a spanner instead of driver
+		// Uses the obj_tool that is already created, but changes the sprite and radius to adjust for it being spanner.
+		if (Play::RandomRoll(2) == 1)
+		{
+			Play::SetSprite(obj_tool, "spanner", 0);
+			obj_tool.radius = 100;
+			obj_tool.velocity.x = -4;
+			obj_tool.rotSpeed = 0.1f;
+		}
+		Play::PlayAudio("tool");
+	}
+	if (Play::RandomRoll(150) == 1) 
+	{
+		int id = Play::CreateGameObject(TYPE_COIN, obj_fan.pos, 40, "coin");
+		GameObject& obj_coin = Play::GetGameObject(id);
+		obj_coin.velocity = { -3, 0 };
+		obj_coin.rotSpeed = 0.1f;
+	}
+
+	Play::UpdateGameObject(obj_fan);
+
+	if (Play::IsLeavingDisplayArea(obj_fan))
+	{
+		obj_fan.pos = obj_fan.oldPos;
+		obj_fan.velocity.y *= -1;
+	}
+}
+
+void UpdateTools() 
+{
+	GameObject& obj_agent8 = Play::GetGameObjectByType(TYPE_AGENT8);
+	std::vector<int> vTools = Play::CollectGameObjectIDsByType(TYPE_TOOL);
+
+	for (int id : vTools)
+	{
+		GameObject& obj_tool = Play::GetGameObject(id);
+
+		if (Play::IsColliding(obj_tool, obj_agent8))
+		{
+			Play::StopAudioLoop("music");
+			Play::PlayAudio("die");
+			obj_agent8.pos = { -100, -100 };
+		}
+		Play::UpdateGameObject(obj_tool);
+
+		if (Play::IsLeavingDisplayArea(obj_tool, Play::VERTICAL))
+		{
+			obj_tool.pos = obj_tool.oldPos;
+			obj_tool.velocity.y *= -1;
+		}
+
+		Play::DrawObjectRotated(obj_tool);
+
+		//Once it goes out of playable area, destroys the object
+		if (!Play::IsVisible(obj_tool))
+		{
+			Play::DestroyGameObject(id);
+		}
+	}
+}
+
